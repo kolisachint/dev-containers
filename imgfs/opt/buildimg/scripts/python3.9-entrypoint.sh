549437580e3c7c7716debc2924dd533366246715
Sachin Koli <kolisachint@gmail.com>
2021-09-20 18:26:10 +0530

    python dev container

---
+++ imgfs/opt/buildimg/scripts/python3.9-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# User-provided configuration must always be respected.
+
+if [ ! -e "/root/.ssh" ]; then
+    mkdir /root/.ssh
+fi
+
+ssh-keyscan `echo ${GIT_SRC} | cut -d ':' -f1|cut -d '@' -f2 ` >> ~/.ssh/known_hosts
+
+mkdir /appl
+cd /appl
+git clone ${GIT_SRC} --config core.sshCommand="ssh -i /my-secrets/my-private-ssh-key"
+
+cd `echo ${GIT_SRC} | cut   -d ':' -f2 |cut -d '/' -f2 |cut -d '.' -f1`
+
+if [ -e "requirements.txt" ]; then
+    pip3 --disable-pip-version-check --no-cache-dir \
+      install -r requirements.txt
+fi
+
+tail -f /dev/null
+
